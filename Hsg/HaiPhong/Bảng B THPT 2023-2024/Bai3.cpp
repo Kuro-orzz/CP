667fdc9254b991fb97f209ad36d6fb62d0f45be6
Kuro-orzz <23020674@vnu.edu.vn>
2025-03-15 00:54:10 +0700

    update

--- Hsg/HaiPhong/Bảng B THPT 2023-2024/Bai3.cpp
+++ Hsg/HaiPhong/Bảng B THPT 2023-2024/Bai3.cpp
@@ -16,19 +16,54 @@
 #include <climits>
 #define filein() freopen("test.inp", "r", stdin);
 #define fileout() freopen("test.out", "w", stdout);
-#define fast() ios_base::sync_with_stdio(NULL);cout.tie(NULL);cin.tie(NULL);
+#define fast() ios_base::sync_with_stdio(NULL);cin.tie(NULL);
 #define ll long long
 #define MOD 1000000007
-#define N 100000
+#define N 10000000
 using namespace std;
 
+bool nt[N+1];
+
+void snt(){
+    memset(nt, true, sizeof(nt));
+    nt[0] = nt[1] = false;
+    for(int i = 2; i <= sqrt(N); i++)
+        if(nt[i]){
+            int j = i * i;
+            while(j <= N){
+                nt[j] = false;
+                j += i;
+            }
+        }
+}
+
 void solve(){
-    string s; cin >> s;
-    int cnt = 0;
-    for(int i = 0; i < (int)s.length(); i++)
-        if(s[i] >= '0' && s[i] <= '9')
-            cnt++;
-    cout << cnt;
+    int n, m; cin >> n >> m;
+    int a[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    snt();
+    int f[n];
+    f[0] = a[0];
+    for(int i = 1; i < n; i++)
+        f[i] = f[i-1] + a[i];
+    while(m--){
+        int x, y; cin >> x >> y;
+        if(x == 1){
+            int k = f[y-1];
+            if(k > 0 && nt[k])
+                cout << 1 << '\n';
+            else 
+                cout << 0 << '\n';
+        }
+        else{
+            int k = f[y-1] - f[x-2];
+            if(k > 0 && nt[k])
+                cout << 1 << '\n';
+            else 
+                cout << 0 << '\n';
+        }
+    }
 }
 
 int main(){
